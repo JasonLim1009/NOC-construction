34fcc8d5642d7d767773f3bf42d94bc86a3d44c3
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2025-10-20 00:54:17 +0800

    Delete assets directory

--- assets/QC/QC Checker _H.bat
+++
@@ -1,8 +0,0 @@
-start %systemdrive%"%homepath%"\Desktop\QC\"song.MP4"
-start %systemdrive%"%homepath%"\Desktop\QC\Battery\"BatteryInfoView.exe"
-start %systemdrive%"%homepath%"\Desktop\QC\"white spot check.jpg"
-start %systemdrive%"%homepath%"\Desktop\QC\"Keyboard Keys Checker.exe"
-start microsoft.windows.camera:
-control mmsys.cpl sounds
-dxdiag
-diskmgmt.msc

4b31200feb0c33fa0b45eabe1985c0bf7519905d
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2025-10-20 00:47:19 +0800

    Add files via upload

---
+++ assets/QC/QC Checker _H.bat
@@ -0,0 +1,8 @@
+start %systemdrive%"%homepath%"\Desktop\QC\"song.MP4"
+start %systemdrive%"%homepath%"\Desktop\QC\Battery\"BatteryInfoView.exe"
+start %systemdrive%"%homepath%"\Desktop\QC\"white spot check.jpg"
+start %systemdrive%"%homepath%"\Desktop\QC\"Keyboard Keys Checker.exe"
+start microsoft.windows.camera:
+control mmsys.cpl sounds
+dxdiag
+diskmgmt.msc
